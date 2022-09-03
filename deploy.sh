75e91ee1a92ccc759746e566bd107e22a9fab119
Adam Hayward <adam@cuskit.co.uk>
2022-09-03 22:35:01 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
-deployTo=~/public_html
-rm -rf $deployTo/*
+deployTo=~/adam-hayward.co.uk
+cd $deployTo
+rm -rf !(".env")
 /bin/rsync --delete -r -exclude=.git,.cpanel.yml,.externalScripts ~/repositories/adam-hayward/* $deployTo
 
 echo Deployed Source
 cd $deployTo
 
 echo Installing Dependancies
-composer install
+composer install --optimize-autoloader --no-dev
 npm install
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan migrate --force
